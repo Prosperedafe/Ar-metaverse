ceea994025aa95abcd6a1ec125bcc41545bcf4cb
prosper edafe <mamusedafe19@gmail.com>
2023-01-12 23:39:02 +0100

    adjustment

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const open = document.getElementById('open');
 const menu = document.getElementById('menu')
-open.addEventListener('click', function () {
+open.addEventListener('click', () => {
     menu.classList.toggle('menu-open')
 })
+console.log(menu);
